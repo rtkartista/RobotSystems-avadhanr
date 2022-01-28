b522e6ed11a238d7d6119bcecc9ade91213594da
rtkartista <ritika.avadhanula@gmail.com>
2022-01-28 05:29:20 -0800

    week3

--- week3/picarxy_new.py
+++ week3/picarxy_new.py
@@ -2,7 +2,10 @@
 import logging
 import logdecorator
 import atexit
-
+import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import numpy as np
 import time
 try :
     from ezblock import *
@@ -242,6 +245,10 @@ class Sensors(object):
         adc_value_list.append(self.S2.read())
         return adc_value_list
 
+color_dict = {'red':[0,4],'orange':[5,18],'yellow':[22,37],'green':[42,85],'blue':[92,110],'purple':[115,165],'red_2':[165,180]}  #Here is the range of H in the HSV color space represented by the color
+
+kernel_5 = np.ones((5,5),np.uint8) #Define a 5×5 convolution kernel with element values of all 1.
+
 class Interpreter(object):
     # dark = 1, light = 0
     # polarity: line the system follows - higher sensitivity - polarity 0
@@ -249,50 +256,39 @@ class Interpreter(object):
         self.polarity = polarity
         self.sensitivity = sensitivity
 
-    def get_line_status(self,fl_list):
-        d = dict(); 
-        # add code regarding polarity
-        # l l l
-        if fl_list[0] > self.sensitivity and fl_list[1] > self.sensitivity and fl_list[2] > self.sensitivity:
-            diff = fl_list[1] - fl_list[2]
-            if self.polarity == 0:
-                d['str'] = 'stop'
-            else:
-                d['str'] = 'forward'
-            d['diff'] = diff
-            return d
-        # d d d    
-        elif fl_list[0] <= self.sensitivity and fl_list[1] <= self.sensitivity and fl_list[2] <= self.sensitivity:
-            diff = fl_list[1] - fl_list[2]
-            if self.polarity == 0:
-                d['str'] = 'forward'
-            else:
-                d['str'] = 'stop'
-            d['diff'] = diff
-            return d
-        # l l d
-        # l d d
-        elif (fl_list[0] > self.sensitivity and fl_list[1] > self.sensitivity and fl_list[2] <= self.sensitivity) or \
-        (fl_list[0] > self.sensitivity and fl_list[1] <= self.sensitivity and fl_list[2] <= self.sensitivity):
-            diff = fl_list[1] - fl_list[2]
-            if self.polarity == 0:
-                d['str'] = 'right'
-            else:
-                d['str'] = 'left'
-            d['diff'] = diff
-            return d
-        # d d l
-        # d l l
-        elif (fl_list[0] <= self.sensitivity and fl_list[1] <= self.sensitivity and fl_list[2] > self.sensitivity) or \
-            (fl_list[0] <= self.sensitivity and fl_list[1] > self.sensitivity and fl_list[2] > self.sensitivity):
-            diff = fl_list[1] - fl_list[2]
-            if self.polarity == 0:
-                d['str'] = 'left'
-            else:
-                d['str'] = 'right'
-            d['diff'] = diff
-            return d
-         
+    def color_detect(img,color_name):
+
+    # The blue range will be different under different lighting conditions and can be adjusted flexibly.  H: chroma, S: saturation v: lightness
+    resize_img = cv2.resize(img, (160,120), interpolation=cv2.INTER_LINEAR)  # In order to reduce the amount of calculation, the size of the picture is reduced to (160,120)
+    hsv = cv2.cvtColor(resize_img, cv2.COLOR_BGR2HSV)              # Convert from BGR to HSV
+    color_type = color_name
+    
+    mask = cv2.inRange(hsv,np.array([min(color_dict[color_type]), 60, 60]), np.array([max(color_dict[color_type]), 255, 255]) )           # inRange()：Make the ones between lower/upper white, and the rest black
+    if color_type == 'red':
+            mask_2 = cv2.inRange(hsv, (color_dict['red_2'][0],0,0), (color_dict['red_2'][1],255,255)) 
+            mask = cv2.bitwise_or(mask, mask_2)
+
+    morphologyEx_img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel_5,iterations=1)              # Perform an open operation on the image 
+
+    contours, hierarchy = cv2.findContours(morphologyEx_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)          # Find the contour in morphologyEx_img, and the contours are arranged according to the area from small to large
+
+    color_area_num = len(contours) # Count the number of contours
+
+    if color_area_num > 0: 
+        for i in contours:    # Traverse all contours
+            x,y,w,h = cv2.boundingRect(i)      # Decompose the contour into the coordinates of the upper left corner and the width and height of the recognition object
+
+            # Draw a rectangle on the image (picture, upper left corner coordinate, lower right corner coordinate, color, line width)
+            if w >= 8 and h >= 8: # Because the picture is reduced to a quarter of the original size, if you want to draw a rectangle on the original picture to circle the target, you have to multiply x, y, w, h by 4.
+                x = x * 4
+                y = y * 4 
+                w = w * 4
+                h = h * 4
+                cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)  # Draw a rectangular frame
+                cv2.putText(img,color_type,(x,y), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255),2)# Add character description
+
+    return img,mask,morphologyEx_img
+     
         # robust to lighting conditions
         # [-1, 1] with respect to a edge detected
         # +1 = line left to the robot
@@ -341,6 +337,29 @@ if __name__ == "__main__":
     im = Interpreter(200)
     px = Picarx()
     cx = Controller(px)
+
+    #init camera
+    print("start color detect")
+    camera = PiCamera()
+    camera.resolution = (640,480)
+    camera.framerate = 24
+    rawCapture = PiRGBArray(camera, size=camera.resolution)  
+
+
+    for frame in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):# use_video_port=True
+        img = frame.array
+        img,img_2,img_3 =  color_detect(img,'red')  # Color detection function
+        cv2.imshow("video", img)    # OpenCV image show
+        cv2.imshow("mask", img_2)    # OpenCV image show
+        cv2.imshow("morphologyEx_img", img_3)    # OpenCV image show
+        rawCapture.truncate(0)   # Release cache
+    
+        k = cv2.waitKey(1) & 0xFF
+        # 27 is the ESC key, which means that if you press the ESC key to exit
+        if k == 27:
+            camera.close()
+            break
+
     while True:
         time.sleep(1)
         sm_val_list = sm.get_adc_value()
