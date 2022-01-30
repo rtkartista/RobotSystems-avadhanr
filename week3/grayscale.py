2d8ceea5157fd98a8f7b62a31eff36443ceae944
rtkartista <ritika.avadhanula@gmail.com>
2022-01-29 21:48:24 -0800

    uploading the unclaibrated code

--- week3/grayscale.py
+++ week3/grayscale.py
@@ -10,19 +10,9 @@ class Interpreter(object):
         all_vals = np.array(all_vals)
         mean = all_vals.mean(axis=0)
         std = all_vals.std(axis=0)
-
-        #assume gaussian noise and accept values within 1 std of the mean as that sensor value. 
         return (mean, std)
     
-    #main sensing loop
     def get_line_status(self,fl_list):
-        # roboustness check(can we rely on the data recieved), is the car slightly away from the mean or largely away 
-        # take the 95% confidence interval calculate z-score
-        polarity = fl_list[2]
-        z = (self.ref-fl_list[0])/fl_list[1]
-        if(z>2.0): 
-            print("please keep the car aligned to the tape")
-            return "stop"
         #threshold for value robustness = 5
         threshold = 3
         
