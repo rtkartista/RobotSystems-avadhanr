cb722728c377eecacf6168a1c5de640ef5fa4882
rtkartista <ritika.avadhanula@gmail.com>
2022-02-03 13:40:44 -0800

    grayscale+week4+nocalibration

--- week4/interpretor.py
+++ week4/interpretor.py
@@ -1,33 +1,42 @@
 from picarxy_new import *
 import numpy as np
 class Interpreter(object):
-    def __init__(self,ref = 1000):
+    def __init__(self,ref = 1000, msg_calib = [10, 500]):
         self.ref = ref
-
-    def calibrate(self):
-        all_vals = []
-        for i in range(100): 
-            all_vals.append(self.get_grayscale_data())
-        all_vals = np.array(all_vals)
-        mean = all_vals.mean(axis=0)
-        std = all_vals.std(axis=0)
-        return (mean, std)
+        self.min = msg_calib[0]
+        self.max = msg_calib[1]
+        self.polarity = 0 # follow the light line
     
-    def get_line_status(self,fl_list):
+    def get_line_status(self,fl_list, calib_msg):
         #threshold for value robustness = 5
         threshold = 3
-        
+        # light
+        if self.polarity == 0:
+            if fl_list[0] > self.ref  and fl_list[1] > self.ref and fl_list[2] > self.ref:
+                return 'stop'
+            elif (fl_list[1]-self.ref) <= threshold:
+                print("forward")
+                return 'forward'
+            
+            elif (fl_list[0]-self.ref) <= threshold:
+                print("right")
+                return 'right'
 
-        if fl_list[0] > self.ref  and fl_list[1] > self.ref and fl_list[2] > self.ref:
-            return 'stop'
-        elif (fl_list[1]-self.ref) <= threshold:
-            print("forward")
-            return 'forward'
-        
-        elif (fl_list[0]-self.ref) <= threshold:
-            print("right")
-            return 'right'
+            elif (fl_list[2]-self.ref) <=threshold :
+                print("left")
+                return 'left'
+        # dark
+        else:
+            if fl_list[0] > self.ref  and fl_list[1] > self.ref and fl_list[2] > self.ref:
+                return 'stop'
+            elif (fl_list[1]-self.ref) <= threshold:
+                print("forward")
+                return 'forward'
+            
+            elif (fl_list[0]-self.ref) <= threshold:
+                print("right")
+                return 'right'
 
-        elif (fl_list[2]-self.ref) <=threshold :
-            print("left")
-            return 'left'
+            elif (fl_list[2]-self.ref) <=threshold :
+                print("left")
+                return 'left'
--- week4/sensors.py
+++ week4/sensors.py
@@ -1,5 +1,5 @@
 from picarxy_new import *
-
+import numpy as np
 class Sensors(object):
     def __init__(self):
         self.camera_servo_pin1 = Servo(PWM('P0'))
@@ -39,6 +39,17 @@ class Sensors(object):
         self.camera_servo_pin2.angle(-1*(value + -1*self.cam_cal_value_2))
         # print("self.cam_cal_value_2:",self.cam_cal_value_2)
         print((value + self.cam_cal_value_2))
+
+    def calibrate(self):
+        all_vals = []
+        for i in range(100): 
+            all_vals.append(self.get_adc_value())
+        all_vals = np.array(all_vals)
+        min = all_vals[0].mean(axis=0)
+        max = (all_vals[1].mean(axis=0) + all_vals[2].mean(axis=0)) / 2
+
+        #assume gaussian noise and accept values within 1 std of the mean as that sensor value. 
+        return [min, max]
 
     def get_adc_value(self):
         adc_value_list = []
